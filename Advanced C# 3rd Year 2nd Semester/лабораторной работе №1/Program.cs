fbfad7f8e6246c66d232cd2b43e79efd52fe22a2
Dry english salim <45246505+MS0C54073@users.noreply.github.com>
2020-07-30 02:42:31 +0300

    Add files via upload

---
+++ Advanced C# 3rd Year 2nd Semester/лабораторной работе №1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chairs, guests, j, c = 1;
+            Console.Write("Enter number of guests");
+            chairs = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number of chairs");
+            guests = Convert.ToInt32(Console.ReadLine());
+            int comb1 = guests - chairs + c;
+            int comb2 = guests - chairs + c;
+            for (int i = 1; i < chairs; i++)
+            {
+                j = comb2 * (comb1 + i);
+                comb2 = j;
+            }
+            Console.Write(Convert.ToString(comb2));
+            Console.ReadLine();
+
+        }
+    }
+}
